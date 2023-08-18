012e634ed52d9e139e5518e07d260e9292149a37
AyEeCy <140329300+AyEeCy@users.noreply.github.com>
2023-08-18 18:09:07 +0300

    Add files via upload

---
+++ 4) Karar Yapıları/Büfe/Proje_notu hesaplama/Proje_notu hesaplama/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje_notu_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double s1, s2, proje, ort;
+            string durum;
+
+            s1 = Convert.ToDouble(textBox1.Text);
+            s2 = Convert.ToDouble(textBox2.Text);
+            proje = Convert.ToDouble(textBox3.Text);
+
+            ort = (s1 + s2 + proje) / 3;
+
+            if (ort >= 60)
+            {
+                durum = "Geçti";
+            }
+
+            else
+            {
+                durum = "Kaldı";
+
+            }
+            textBox4.Text = ort.ToString("0.00") + "  / " + durum;
+        }
+    }
+}
---
+++ 4) Karar Yapıları/Büfe/Sına vnotu hesaplama/Sına vnotu hesaplama/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sına_vnotu_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int  sınav1, sınav2, sınav3, ortalama;
+            sınav1 = Convert.ToInt32(textBox1.Text) ;
+            sınav2 = Convert.ToInt32(textBox2.Text);
+            sınav3 = Convert.ToInt32(textBox3.Text);
+
+            ortalama = (sınav1 + sınav2 + sınav3) / 3;
+
+            textBox4.Text = ortalama.ToString();
+        }
+    }
+}
