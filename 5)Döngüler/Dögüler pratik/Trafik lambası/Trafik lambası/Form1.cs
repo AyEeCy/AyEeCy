ec804094fcb1ba9682b40a2785fffb5478538932
AyEeCy <140329300+AyEeCy@users.noreply.github.com>
2023-08-18 18:16:11 +0300

    Add files via upload

---
+++ 5)Döngüler/Dögüler pratik/Trafik lambası/Trafik lambası/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trafik_lambası
+{
+   
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+            if(sayac == 10)
+            {
+                this.BackColor = Color.Red;
+            }
+            if (sayac == 20)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            if (sayac == 30)
+            {
+                this.BackColor = Color.Green;
+                sayac = 0;
+            }
+        }
+    }
+}
---
+++ 5)Döngüler/Dögüler pratik/Yükleniyor/Yükleniyor/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yükleniyor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+        }
+        int a = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a++;
+            progressBar1.Value = a;
+            label2.Text = a.ToString();
+            if (progressBar1.Value%2 ==  0) {
+                this.BackColor = Color.MidnightBlue;
+                    }
+            if (progressBar1.Value %2 == 1 )
+                this.BackColor = Color.MediumVioletRed;
+            if (progressBar1.Value ==100) {
+                timer1.Stop();
+                MessageBox.Show("İşlem Tamamladı.");
+
+            }
+
+        }
+    }
+}
