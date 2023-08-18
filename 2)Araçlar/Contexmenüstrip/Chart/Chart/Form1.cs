50d9abab6d8fa6e78e84b0af8165ed871d5adcfa
AyEeCy <140329300+AyEeCy@users.noreply.github.com>
2023-08-18 17:51:04 +0300

    Add files via upload

---
+++ 2)Araçlar/Contexmenüstrip/Chart/Chart/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series["Kitaplar"].Points.AddXY("Akdeniz", 15);
+            chart1.Series["Kitaplar"].Points.AddXY("Karadeniz", 5);
+            chart1.Series["Kitaplar"].Points.AddXY("İçAnadolu", 7);
+            chart1.Series["Kitaplar"].Points.AddXY("GüneyDoğu Anadolu", 3);
+            chart1.Series["Kitaplar"].Points.AddXY("Ege", 13);
+        }
+    }
+}
