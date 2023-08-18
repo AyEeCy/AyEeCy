1c10e407b6a3a6e4b39299b9d354842ed59d2ea3
AyEeCy <140329300+AyEeCy@users.noreply.github.com>
2023-08-18 18:19:12 +0300

    Add files via upload

---
+++ 6)Dizi/Diziler/Diziler/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diziler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] adlar = { "ahsen", "ahmet", "mehmet", "filiz", "hüseyin" };
+
+            for (int i = 0; i < adlar.Length; i++){
+                listBox1.Items.Add((b++)+")" + adlar[i]);
+            }
+            
+
+
+        }
+        int a = 0,b = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ 6)Dizi/Foreach/Foreach/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foreach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            int[] sayilar = {32,56,87,54,23,67,78};
+            foreach(int sa in sayilar) {
+
+                listBox1.Items.Add(sa);
+                toplam = toplam + sa;
+                label1.Text = toplam.ToString();
+
+             double  ortalama = toplam/ sayilar.Length;
+                listBox2.Items.Add(ortalama);
+
+             ortalama = toplam / sayilar.Length;
+                label2.Text = ortalama.ToString();
+            }
+
+                
+            }
+        }
+    }
+
